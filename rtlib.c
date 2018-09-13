322457384fa1a54f2cc1a59dff2380847c9eb5d7
Dennis Fischer <dennis.fischer@live.com>
2018-09-13 17:55:37 +0200

    Force exit on error

--- rtlib.c
+++ rtlib.c
@@ -9,42 +9,44 @@
 #define KMAG  "\x1B[35m"
 #define KCYN  "\x1B[36m"
 #define KWHT  "\x1B[37m"
-void guardMe(const unsigned int address, const unsigned int length, const unsigned int expectedHash){
 
-	const unsigned char  *beginAddress = (const unsigned char *)address;
-	unsigned int visited = 0;
-	unsigned char hash = 0;
-	//Note: Length need to be divided by the size of 
-	//type of begin address that we are reading each time, 
-	//otherwise it falls out of the scope (see #3)
+void guardMe(const unsigned int address, const unsigned int length, const unsigned int expectedHash) {
+
+  const unsigned char *beginAddress = (const unsigned char *) address;
+  unsigned int visited = 0;
+  unsigned char hash = 0;
+  //Note: Length need to be divided by the size of
+  //type of begin address that we are reading each time,
+  //otherwise it falls out of the scope (see #3)
 //	printf("%sLength:%d Begin address:%d Expectedhash:%d\n",KRED,length,address,expectedHash);
-	while (visited < length) {
+  while (visited < length) {
 //		printf("%x ",*beginAddress);
-		hash ^= *beginAddress++;
-		++visited;
-	}
+    hash ^= *beginAddress++;
+    ++visited;
+  }
 //	printf("\n");
-	
+
 //	printf("%sruntime hash: %x\n",KGRN,hash);
 //	printf("%sexpected hash: %x\n",KBLU,expectedHash);
 //	printf("%s",KNRM);
-	
-	if (hash !=(unsigned char)expectedHash) {
-		//response();
-		printf("%sTampered binary!\n",KNRM);
 
-		void* callstack[128];
-                int i, frames = backtrace(callstack, 128);
-                char** strs = backtrace_symbols(callstack, frames);
-    
+  if (hash != (unsigned char) expectedHash) {
+    //response();
+    printf("%sTampered binary!\n", KNRM);
 
-                for (i = 0; i < frames; ++i) {
-                        printf("%s\n", strs[i]);
-                }   
+    void *callstack[128];
+    int i, frames = backtrace(callstack, 128);
+    char **strs = backtrace_symbols(callstack, frames);
 
-                free(strs);
-	}
+    for (i = 0; i < frames; ++i) {
+      printf("%s\n", strs[i]);
+    }
+
+    free(strs);
+    exit(777);
+  }
 }
+
 //void respone(){
 //	printf("Tampered binary!");
 //}
@@ -53,5 +55,5 @@ void guardMe(const unsigned int address, const unsigned int length, const unsign
 //        hash +=i;
 //}
 void logHash() {
-	//printf("final hash: %ld\n", hash);
+  //printf("final hash: %ld\n", hash);
 }
